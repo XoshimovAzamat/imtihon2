d6418accab579a75a84f1b1e93e64ed99ed30446
azamat <azamatbekxoshimov571>
2025-04-27 18:07:07 +0500

    deploy

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.routers import DefaultRouter
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 router = DefaultRouter()
 
