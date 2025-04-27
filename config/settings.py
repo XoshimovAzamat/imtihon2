d2f1a72b88531250fc765632d86b3754b01fc536
azamat <azamatbekxoshimov571>
2025-04-27 21:11:49 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-!^a^sdx@n6p(&ga)09=qy9fxc@8e$n+0y+x5bciz_@u*+86z+l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', '161.35.26.88']
+ALLOWED_HOSTS = ['*', '167.71.35.129']
 
 
 # Application definition
