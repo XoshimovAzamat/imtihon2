304b595ad17646e0c04ea393a8e726ceaecc6443
azamat <azamatbekxoshimov571>
2025-04-30 20:52:12 +0500

    t

--- configapp/serializers/group_serializer.py
+++ configapp/serializers/group_serializer.py
@@ -12,3 +12,13 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = "__all__"
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
---
+++ configapp/views/table_view.py
@@ -0,0 +1,24 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from configapp.models import Table
+from configapp.serializers import TableSerializer
+
+
+class TableApi(APIView):
+    @swagger_auto_schema(request_body=TableSerializer)
+    def post(self, request):
+        serializer = TableSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        table_title = Table.objects.all().order_by('-id')
+        paginator = CustomPaginator()
+        paginator.page_size = 2
+        result_page = paginator.paginate_queryset(group_title, request)
+        serializer = TableSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
