0c2d5bc19778e7e6a14148643f4ae399029f86ab
azamat <azamatbekxoshimov571>
2025-05-26 16:29:48 +0500

    contacts

--- config/urls.py
+++ config/urls.py
@@ -28,7 +28,7 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', index, name='index'),
     path('api/', include('configapp.urls')),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
